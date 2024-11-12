c1d26c566d40971fe28e478c1270e60125645aa7
jaccet <155945054+jaccet@users.noreply.github.com>
2024-11-12 15:21:28 +0200

    Added placeholder image and dropdown button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,26 @@
 import './Header.css';
 import React from 'react';
+import account_icon_placeholder from '../images/account_icon_placeholder.png';
 
 // TODO: Add links to different pages
 export default function Header() {
   return (
     <header>
+      <div className='navbar'>
         <div className='links'>
-            <a href='/'>Home</a>
-            <a href='/movies'>Movies</a>
-            <a href='/showtimes'>Showtimes</a>
-            <a href='/favorites'>Favorites</a>
-            <a href='/reviews'>Reviews</a>
-            <a href='/groups'>Groups</a>
+          <a href='/'>Home</a>
+          <a href='/movies'>Movies</a>
+          <a href='/showtimes'>Showtimes</a>
+          <a href='/favorites'>Favorites</a>
+          <a href='/reviews'>Reviews</a>
+          <a href='/groups'>Groups</a>
         </div>
-        <div className='account-icon'>
-            Account icon goes here
+        <div className='dropdown' id='account-icon'>
+          <button className='dropbtn'>
+            <img src={account_icon_placeholder}></img>
+          </button>
         </div>
+      </div>
     </header>
   );
 }
