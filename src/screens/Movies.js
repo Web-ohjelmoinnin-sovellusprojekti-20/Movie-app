7aacce0ae626261598a766406c08fd545c80c9f7
mikkohir <155948800+mikkohir@users.noreply.github.com>
2024-11-19 15:21:52 +0200

    Search toimii

--- src/screens/Movies.js
+++ src/screens/Movies.js
@@ -1,19 +1,36 @@
 import './Movies.css';
 import React, { useState } from 'react';
-import { Nav, Navbar, Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown'
+import { getMovieByName } from '../components/movieAPI';
 
 //TODO: search for movies, if there's one show, if there's none say there's none with such name
 export default function Movies() {
 
   const [movie, setMovie] = useState('')
   const [result, setResult] = useState('')
+  const [moviesData, setmoviesData] = useState('')
 
 
-  function searchHandle (e){
+  const searchHandle = async (e) => {
       e.preventDefault();
-      setResult("You searched for " + movie)
+      setResult("You searched for " + movie + ". Displaying results..")
+
+      try {
+        const data = await getMovieByName(movie)
+        setmoviesData(data)
+        console.log("testi");
+      }
+      catch(error){
+        setmoviesData(null)
+      };
+
   }
+
+  const handleInputChange = (e) => {
+    setMovie(e.target.value);
+  };
+
 
 
   return (
@@ -21,6 +38,8 @@ export default function Movies() {
       <Form onSubmit={searchHandle}>
         <Form.Control type="text" placeholder='Type here...'
         className="mt2"
+        value = {movie}
+        onChange={handleInputChange}
         /> 
       </Form>
       <Form>
@@ -44,6 +63,17 @@ export default function Movies() {
       </Dropdown>
       </Form>
       <label>{result}</label>
+      <pre>
+      {moviesData && (
+        moviesData.map((movie) => (
+        <div>
+        <pre>
+          {JSON.stringify(movie,null,2)}
+        </pre>
+        </div>
+        ))
+      )}
+      </pre>
     </div>
   );
 }
