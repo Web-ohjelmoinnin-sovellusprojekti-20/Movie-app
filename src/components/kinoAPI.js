579c90acff6164459221789feb71296b9ff7529d
Valtteri <valtteri.putaansuu@gmail.com>
2024-11-21 15:23:42 +0200

    All done

--- src/components/kinoAPI.js
+++ src/components/kinoAPI.js
@@ -3,40 +3,66 @@ import { xml2js } from 'xml-js'
 
 const base_url = 'https://www.finnkino.fi/xml'
 
-const getMovieByTheaterAndDate = async(theaterId, date) => { //general finnkino API function format
-    try {
-        const response = await axios.get(base_url + '/Schedule/?area=' + theaterId + '&dt=' + date)
-        const convertToJson = xml2js(response.data, {compact: true, spaces: 2}) //xml - json
-        const xmlDoc = convertToJson.Schedule?.Shows?.Show?.map(show => show.Title?._text) //json filter
-        const noDuplicates = [...new Set(xmlDoc)] //no duplicate movies
-        return noDuplicates
-    } catch (error) {
-        throw error
-    }
-}
-
-const getTheaterName = async(theaterName) => {
-    try {
-        const response = await axios.get(base_url + '/Schedule/?area=' + theaterName)
-        const convertToJson = xml2js(response.data, {compact: true, spaces: 2})
-        const xmlDoc = convertToJson.Schedule?.Shows?.Show?.map(show => show.Theatre?._text)
-        return xmlDoc
-    } catch (error) {
-        throw error
-    }
-}
-
-const getMovieStartTime = async(theaterId, date) => {
+const getAll = async (theaterId, date) => {
     try {
         const response = await axios.get(base_url + '/Schedule/?area=' + theaterId + '&dt=' + date)
         const convertToJson = xml2js(response.data, {compact: true, spaces: 2})
-        const xmlDoc = convertToJson.Schedule?.Shows?.Show?.map(show => show.dttmShowStart?._text)
-        const noDuplicates = [...new Set(xmlDoc)]
-        return noDuplicates
+
+        const xmlDoc = convertToJson.Schedule?.Shows?.Show || []
+
+        const movieData1 = xmlDoc.map(show => ({
+            title: show.Title?._text,
+            image: show.Images?.EventLargeImagePortrait?._text,
+            auditorium: show.TheatreAuditorium?._text,
+            genre: show.Genres?._text || "Not specified",
+            link: show.EventURL?._text || "#",
+            minutes: show.LengthInMinutes?._text,
+            time: show.dttmShowStart?._text,
+            time2: show.dttmShowEnd?._text
+        }))
+
+        const movieData2 = movieData1.map(movie => {
+            let formattedTime = "Not available"
+            let formattedTime2 = "Not available"
+            
+            if (movie.time) {
+                const dateObject = new Date(movie.time)
+                if (!isNaN(dateObject)) {
+                    const hours = dateObject.getHours().toString().padStart(2, '0')
+                    const minutes = dateObject.getMinutes().toString().padStart(2, '0')
+                    formattedTime = (hours + ':' + minutes)
+                }
+            }
+            if (movie.time2) {
+                const dateObject2 = new Date(movie.time2)
+                if(!isNaN(dateObject2)) {
+                    const hours = dateObject2.getHours().toString().padStart(2, '0')
+                    const minutes = dateObject2.getMinutes().toString().padStart(2, '0')
+                    formattedTime2 = (hours + ':' + minutes)
+                }
+            }
+            return { ...movie, formattedTime, formattedTime2}
+        })
+
+        return movieData2
     } catch (error) {
         throw error
     }
 }
 
-export { getMovieByTheaterAndDate, getMovieStartTime, getTheaterName }
+
+//------------------------------------------------------------------------------------
+
+const getDate = () => {
+    const dates = []
+    const today = new Date()
+    for (let i = 0; i < 10; i++) {
+      const date = new Date(today)
+      date.setDate(today.getDate() + i)
+      dates.push(date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear())
+    }
+    return dates
+  }
+
+export { getAll, getDate }
 
--- src/screens/Showtimes.js
+++ src/screens/Showtimes.js
@@ -1,184 +1,123 @@
-import React, { useState } from 'react';
-import { Dropdown } from 'react-bootstrap';
-import { getMovieByTheaterAndDate, getMovieStartTime } from '../components/kinoAPI';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Col, Container, Dropdown, Row } from 'react-bootstrap';
+import { xml2js } from 'xml-js';
+import { getAll, getDate } from '../components/kinoAPI';
 import './Showtimes.css';
 
-export default function Showtimes() {
-  
-  const [theaterData, setTheaterData] = useState(null)
-  const [date, setDate] = useState(null)
-  const [theaterId, setTheaterId] = useState(null)
-  const [theaterName, setTheaterName] = useState(null)
-  const [movieStart, setMovieStart] = useState(null)
+const base_url = 'https://www.finnkino.fi/xml/'
 
-  const getDate = () => {
-    const dates = []
-    const today = new Date()
-    for (let i = 0; i < 10; i++) {
-      const date = new Date(today)
-      date.setDate(today.getDate() + i)
-      dates.push(date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear())
-    }
-    return dates
-  }
+export default function ShowTimes() {
+  const [areas, setAreas] = useState([])
+  const [theater, setTheater] = useState(null)
+  const [theaterName, setTheaterName] = useState(null)
+  const [showingDate, setShowingDate] = useState(null)
+  const [showingData, setShowingData] = useState(null)
 
   const handleKinoData = async() => {
-    if (theaterId && date) {
-      try {
-        const selectedData = await getMovieByTheaterAndDate(theaterId, date)
-        const startTime = await getMovieStartTime(movieStart, date)
-        setTheaterData(selectedData)
-        setMovieStart(startTime)
-      } catch (error) {
-        throw error
-      }
-    } else {
-      alert("Please select a valid theater and date")
-    }
+    const selectedData = await getAll(theater, showingDate)
+    setShowingData(selectedData)
   }
+  const kinoTheaters = (xml) => {
+    const xmlDoc = xml2js(xml, {compact: true})
+    const theaters = xmlDoc.TheatreAreas.TheatreArea
+    const area = []
+
+    for (let i = 0; i < theaters.length; i++) {
+      area.push(
+        {
+          "name": theaters[i].Name._text,
+          "id": theaters[i].ID._text
+        }
+      )
+    }
+    setAreas(area)
+  }
+
+  useEffect(() => {
+    axios
+      .get(base_url + 'TheatreAreas/')
+      .then((response) => kinoTheaters(response.data))
+      .catch((error) => {
+        throw error
+      })
+  }, [])
 
   return (
     <div>
       <div className="dropdown-box">
-      <Dropdown>
-          <Dropdown.Toggle className="dropdown-theaters" variant="success" id="dropdown-theaters">
-            Theaters
+        <Dropdown>
+          <Dropdown.Toggle>
+            Select theater
           </Dropdown.Toggle>
-
           <Dropdown.Menu>
-            
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1012);
-              setTheaterName('Espoo')
-            }}>Espoo</Dropdown.Item>
-            
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1039);
-              setTheaterName('Espoo: OMENA')
-            }}>Espoo: OMENA</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1038);
-              setTheaterName('Espoo: SELLO')
-            }}>Espoo: SELLO</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1002);
-              setTheaterName('Helsinki')
-            }}>Helsinki</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1045);
-              setTheaterName('Helsinki: ITIS')
-            }}>Helsinki: ITIS</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1032);
-              setTheaterName('Helsinki: MAXIM')
-            }}>Helsinki: MAXIM</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1033);
-              setTheaterName('Helsinki: TENNISPALATSI')
-            }}>Helsinki: TENNISPALATSI</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1013);
-              setTheaterName('Vantaa: FLAMINGO')
-            }}>Vantaa: FLAMINGO</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1015);
-              setTheaterName('Jyäskylä: FANTASIA')
-            }}>Jyväskylä: FANTASIA</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1016);
-              setTheaterName('Kuopio: SCALA')
-            }}>Kuopio: SCALA</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1017);
-              setTheaterName('Lahti: KUVAPALATSI')
-            }}>Lahti: KUVAPALATSI</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1041);
-              setTheaterName('Lappeenranta: STRAND')
-            }}>Lappeenranta: STRAND</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1018);
-              setTheaterName('Oulu: PLAZA')
-            }}>Oulu: PLAZA</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1019);
-              setTheaterName('Pori: PROMENADI')
-            }}>Pori: PROMENADI</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1021);
-              setTheaterName('Tampere')
-            }}>Tampere</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1034);
-              setTheaterName('Tampere: CINE ATLAS')
-            }}>Tampere: CINE ATLAS</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1035);
-              setTheaterName('Tampere: PLEVNA')
-            }}>Tampere: PLEVNA</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1047);
-              setTheaterName('Turku ja Raisio')
-            }}>Turku ja Raisio</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1022);
-              setTheaterName('Turku: KINOPALATSI')
-            }}>Turku: KINOPALATSI</Dropdown.Item>
-
-            <Dropdown.Item onClick={() => {
-              setTheaterId(1046);
-              setTheaterName('Raisio: LUXE MYLLY')
-            }}>Raisio: LUXE MYLLY</Dropdown.Item>
-
+            {areas.map((area, index) => (
+              <Dropdown.Item key={index} onClick={() => {
+                setTheater(area.id)
+                setTheaterName(area.name)
+              }}>{area.name}</Dropdown.Item>
+            ))}
           </Dropdown.Menu>
-
         </Dropdown>
 
-        <Dropdown> 
-      
-          <Dropdown.Toggle className="dropdown-dates" variant="info" id="dropdown-dates">
-            Dates
+        <Dropdown>
+          <Dropdown.Toggle>
+            Select date
           </Dropdown.Toggle>
-      
-    
-      
-
-        <Dropdown.Menu>
-          {getDate().map((date, index) => (
-            <Dropdown.Item key={index} onClick={() => setDate(date)}>{date}</Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      
-      </Dropdown>
-      </div>
-      <button className="button" onClick={handleKinoData}>Search</button>
-      {theaterData && (
-        <div>
-          <h2>Movies:</h2>
-          <ul className="movies-box">
-            {theaterData.map((theaterData, index) => (
-              <li key={index}>{theaterData} | <strong>{theaterName} |</strong></li>
+          <Dropdown.Menu>
+            {getDate().map((date, index) => (
+              <Dropdown.Item key={index} onClick={() => {
+                setShowingDate(date)
+              }}>{date}</Dropdown.Item>
             ))}
-          </ul>
-        </div>
-      )}
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Button variant="secondary" onClick={() => {
+          handleKinoData()
+        }}>Search showings</Button>
     </div>
-  );
+      <div>
+        <Container>
+          {showingData && (
+            <Row>
+              {showingData.map((movie, index) => (
+                <Col lg={3} md={3} sm={6} xs={12} key={index}>
+                  <Card>
+                    <Card.Img src={movie.image} fluid></Card.Img>
+                    <br/>
+                    <Card.Body className="showings">
+                      <Card.Title>
+                        <u>
+                          <a
+                            href={movie.link}
+                            target="_blank"
+                          >{movie.title}</a></u>
+                        </Card.Title>
+                      <br/>
+                      <Card.Text>
+                        <strong>Theatre: {theaterName || "Select theatre"}</strong>
+                        <br/><br/>
+                        <strong>Auditorium: {movie.auditorium || "N/A"}</strong>
+                        <br/><br/>
+                        <strong>Date: {showingDate || "Select date"}</strong>
+                        <br/><br/>
+                        <strong>Starts: {movie.formattedTime}</strong>
+                        <br/><br/>
+                        <strong>Ends: {movie.formattedTime2}</strong>
+                        <br/><br/>
+                        <strong>Length: {movie.minutes} minutes</strong>
+                        <br/><br/>
+                        <strong>Genre: {movie.genre || "N/A"}</strong>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          )}
+        </Container>
+      </div>
+    </div>
+  )
 }
