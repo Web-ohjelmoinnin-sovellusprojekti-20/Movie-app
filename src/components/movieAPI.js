aa6befc70a617ccc985d882b0cffc0b176454a7c
mikkohir <155948800+mikkohir@users.noreply.github.com>
2024-12-03 10:11:54 +0200

    Movies almost done, pages in progress

--- src/components/movieAPI.js
+++ src/components/movieAPI.js
@@ -5,8 +5,9 @@ const key = '&api_key=e39b43c4f5b8bda212cacb543bd29bad'
 // const url = 'https://api.themoviedb.org/3/search/movie?query=lion&language=en-US&page=1';
 // https://developer.themoviedb.org/reference/search-movie KESKEINEN!
 
-const getMovieByName = async (name, genreIds) => {
+const getMovieByName = async (name, genreIds, Page) => {
     try {
+        console.log(genreIds)
         const response = await axios.get(url + name + '&language=en-US&page=1' + key )
         let movies = response.data.results
         if (genreIds.length > 0) {
