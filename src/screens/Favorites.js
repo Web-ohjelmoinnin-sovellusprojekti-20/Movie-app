5d43eb09285184185a134edb3d1850692910937e
Valtteri <valtteri.putaansuu@gmail.com>
2024-12-09 12:05:44 +0200

    pagination logic

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Accordion, Button, Image } from 'react-bootstrap';
+import { Accordion, Container, Image, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import PaginationComp from '../components/Pagination.js';
 import account_icon_placeholder from '../images/account_icon_placeholder.png';
 import './Favorites.css';
 
 export default function Favorites() {
   
   const [favouriteData, setFavouriteData] = useState([])
+  const [page, setPage] = useState(1)
+
+  const itemsPerPage = 5
+  const startIndex = (page - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const paginatedData = favouriteData.slice(startIndex, endIndex)
 
   useEffect(() => {
     const getFavouriteMovies = async() => {
@@ -43,28 +50,48 @@ const addMovieToUser = (email, movies) => {
 }
 
   return (
-
     <div>
       <h1>Shared favourite movies</h1>
-      
-      <Accordion className="whole-box">
-        {favouriteData.map((favourite, index) => (
-          <Accordion.Item className="accordion-box" eventKey={index.toString()} key={index}>
-            <Accordion.Header>
-              <Image src={account_icon_placeholder} roundedCircle></Image>
-              {favourite.email}'s list of favourite movies
-            </Accordion.Header>
-            <Accordion.Body>
-                {favourite.movie.map((movie, idx) => (
-                  <strong><li key={idx}>{movie}</li></strong>
-                ))}
-                <br/>
-                <Button>View profile</Button>
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
 
+      {favouriteData.length === 0 ? (
+        <p>No favourite movies available.</p>
+      ) : (
+        <Accordion className="whole-box">
+          {paginatedData.map((favourite, index) => (
+            <Accordion.Item className="accordion-box" eventKey={index.toString()} key={index}>
+              <Accordion.Header>
+                <Image src={account_icon_placeholder} roundedCircle></Image>
+                {favourite.email}'s list of favourite movies
+              </Accordion.Header>
+              <Accordion.Body>
+                {favourite.movie.map((movie, idx) => (
+                  <strong key={idx}>
+                    <li>{movie}</li>
+                  </strong>
+                ))}
+                <Navbar className="navBox">
+                  <Container>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                      <Nav>
+                        <NavDropdown>
+                          <NavDropdown.Item href="/account">View account</NavDropdown.Item>
+                        </NavDropdown>
+                      </Nav>
+                    </Navbar.Collapse>
+                  </Container>
+                </Navbar>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
+
+      <PaginationComp
+        total={Math.ceil(favouriteData.length / itemsPerPage)}
+        current={page}
+        onChange={(newPage) => setPage(newPage)}
+      />
     </div>
   )
 }
