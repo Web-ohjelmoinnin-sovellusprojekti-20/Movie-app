613ffdfa776d0da021b275f4a679b05eaf744b97
jaccet <155945054+jaccet@users.noreply.github.com>
2024-12-12 14:22:43 +0200

    Refactored favorite

--- server/controllers/FavouriteController.js
+++ server/controllers/FavouriteController.js
@@ -1,49 +1,34 @@
-import { pool } from '../helpers/db.js';
+import { appendToFavorite, deleteFromFavorite, getVisibleFavorites } from '../models/Favorite.js';
 
 
-const getFavourites = async(req, res) => {
+const getFavourites = async(req, res, next) => {
     try {
-        const query = `
-            SELECT email, UNNEST(movie_name) AS movie_name
-            FROM favorite
-            WHERE current_visibility = 'VISIBLE';
-        `
-        const result = await pool.query(query)
-        res.status(200).json(result.rows)
-        console.log(query, result.rows)
+        const result = await getVisibleFavorites();
+        return res.status(200).json(result.rows)
     } catch (error) {
-        res.status(500).json({error})
+        return next(error);
     }
 }
 
-const appendList = async(req, res) => {
+const appendList = async(req, res, next) => {
     try {
         const { email, movie_name } = req.body
-        const query = `
-            UPDATE favorite
-            SET movie_name = array_append(movie_name, $1),
-                current_visibility = 'VISIBLE'
-            WHERE email = $2
-        `
-        await pool.query(query, [movie_name, email])
-        res.status(200).json({ message: "Favourite list updated succesfully"})
+        
+        await appendToFavorite(email,movie_name);
+        return res.status(200).json({ message: "Favourite list updated succesfully"})
     } catch (error) {
-        res.status(500).json({error})
+        return next(error);
     }
 }
 
-const removeFromList = async(req, res) => {
+const removeFromList = async(req, res, next) => {
     try {
         const { email, movie_name } = req.body
-        const query = `
-            UPDATE favorite
-            SET movie_name = array_remove(movie_name, $1)
-            WHERE email = $2
-        `
-        await pool.query(query, [movie_name, email])
-        res.status(200).json({ message: "Movie succesfully removed from favourites"})
+        
+        await deleteFromFavorite(email, movie_name);
+        return res.status(200).json({ message: "Movie succesfully removed from favourites"})
     } catch (error) {
-        res.status(500).json({error})
+        return next(error);
     }
 }
 
